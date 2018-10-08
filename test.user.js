638342bc23d80cc4d2e1d6d9f802b69f7cdec7f3
a63420000 <sylphis00522@gmail.com>
2018-10-08 15:38:30 +0800

    Add test.user

---
+++ test.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         test
+// @namespace    http://github.com/a63420000/TestForTamperMonkey
+// @version      0.1
+// @description  An extended pixiv multiple tools for downloading specific graphs by setting user own rules
+// @description  This extended feature is based on XZ Pixiv Batch Downloader to add another feature to fit my own needs
+// @description  I want to set a ruler that can download works that numbers are less than threshold
+// @author       a63420000
+// @licence      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
+// @icon        https://www.pixiv.net/favicon.ico
+// @match        http://*/*
+// @grant       GM_xmlhttpRequest
+// @connect     i.pximg.net
+// @connect     i1.pixiv.net
+// @connect     i2.pixiv.net
+// @connect     i3.pixiv.net
+// @connect     i4.pixiv.net
+// @connect     i5.pixiv.net
+// @connect     imgaz.pixiv.net
+// ==/UserScript==
+
+
+/*
+
+(0.1) This version is just to test how to use tampermonkey for writing script
+
+(2018/10/8 update)
+
+*/
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
